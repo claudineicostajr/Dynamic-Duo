a425d3c34f9eb0c9b862aca42910d72d424e9cde
claudineicostajr <101840541+claudineicostajr@users.noreply.github.com>
2022-06-30 20:20:33 -0300

    Contruindo consulta individuo

--- PHP/inividuosPHP/consultaInviduoForm.php
+++ PHP/inividuosPHP/consultaInviduoForm.php
@@ -68,12 +68,45 @@
                     <img class="icone" id="btnEditarIndividuo" src="assets/IMGs/icons/pencil.png" alt="">
                 </td>
             </tr>
+            <?php
+				require_once("../conexaoBanco.php");
+				$comando="SELECT c.*, t.alterego FROM individuos";
+
+				if(isset($_GET['pesquisa']) && $_GET['pesquisa']!=""){
+					$comando.=" WHERE c.alterego LIKE '".$_GET['pesquisa']."%' OR c.local LIKE '".$_GET['pesquisa']."%' OR t.alterego LIKE '".$_GET['pesquisa']."%'";
+				}
+				//echo $comando;
+				$resultado=mysqli_query($conexao,$comando);
+				$individuos=array();
+				$linhas=mysqli_num_rows($resultado);
+				
+				if($linhas==0){
+					echo "<tr><td colspan='12'>Nenhum compromisso encontrado!</td></tr>";
+				}else{		
+
+				while($c = mysqli_fetch_assoc($resultado)){
+					array_push($individuos, $c);
+				}
+
+				foreach($individuos as $c){
+                    echo "<td>".$c['imagemPerfil']."</td>";
+					echo "<td>".$c['alterego']."</td>";
+					echo "<td>".$c['nome']."</td>";
+					echo "<td>".$c['afiliacao']."</td>";
+					echo "<td>".$c['habilidades']."</td>";
+					echo "<td>".$c['moralidade']."</td>";					
+					echo "<td> Santa Catarina</td>";
+					echo "<td>".$c['numero']."</td>";				
+			?>
         </tbody>
     </div> 
     </table>
     <br>
 </div>
-
+?php 
+						} //fechamwnto do foreach
+					} //fechamento do else
+				?>
 </body>
 <script src="assets/JS/jquery-3.6.0.min.js"></script>
 <script src="assets/JS/script.js"></script>
