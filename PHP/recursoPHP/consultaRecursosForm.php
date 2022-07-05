0ad9714ecef3aba414abbe3e2323770f9f7fcf21
claudineicostajr <101840541+claudineicostajr@users.noreply.github.com>
2022-07-05 19:51:35 -0300

    modificações basicas

--- PHP/recursoPHP/consultaRecursosForm.php
+++ PHP/recursoPHP/consultaRecursosForm.php
@@ -12,6 +12,28 @@
     <title>Consulta de recursos</title>
 </head>
 <body>
+        <?php if(isset($_GET['retorno'])==true && $_GET['retorno']==0){ ?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <span>Recurso irá ser usado em alguma missao, não pode ser excluido!</span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+        <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==1){ ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span>Recurso cadastrado com sucesso!</span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+        <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==2){ ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span>Recurso exluído com sucesso!</span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+        <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==3){ ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span>Falha ao excluir!</span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+        <?php } ?>
+        
     <form action="#" method="GET">
         <div id="pesquisaRecursos">
             <input type="text" name="pesquisa" id="pesquisaRecursosInput" placeholder="Busca por recurso">
@@ -30,7 +52,7 @@
                     Descrição do recurso
                 </td>
                 <td>
-                    Excluir
+                    Ação
                 </td>
             </tr>
             
@@ -61,7 +83,14 @@
                     echo "<td>".$r['descricao']."</td>";
             ?> 
                 <td>
-                <!-- formulário de exclusão ediçao -->
+                <form action="excluirRecurso.php" method="POST" class="formAcao">
+					<input type="hidden" name="idRecurso" value="<?=$r['idRecurso']?>">
+					<button type="submit" class="botaoAcao">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+						<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+						</svg>
+					</button>				
+				</form>
                 </td>
                 </tr>
 
